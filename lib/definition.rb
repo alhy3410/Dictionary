fcf0b3d54785b8d126f03ebd38d0a8f4cc5322f9
Andrew Lee <student@epicodus.com>
2015-05-01 09:30:03 -0700

    add an all function so that you can see all definitions

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,8 +1,13 @@
 class Definition
-
+  @@all_definitions = []
   attr_reader(:make_definition)
 
   define_method(:initialize) do |attributes|
-    @make_definition = attributes[:definition]
+    @make_definition = attributes[:make_definition]
   end
+
+  define_singleton_method(:all) do
+    @@all_definitions
+  end
+
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,11 +2,30 @@ require('rspec')
 require('definition')
 
 describe('Definitions') do
+  #
+  # before() do
+  #   Definitions.clear()
+  # end
+
 
   describe('#make_definition') do
     it("will make a definition for the word") do
-      new_definition = Definition.new({:definition => "This is definition 1"})
+      new_definition = Definition.new({:make_definition => "This is definition 1"})
       expect(new_definition.make_definition()).to(eq("This is definition 1"))
+    end
+  end
+
+  # describe('#save') do
+  #   it('will save a definition to the array') do
+  #     new_definition = Definition.new({:make_definition => "This is definition 1"})
+  #     new_definition.save()
+  #     expect(Definition.all()).to(eq[new_definition])
+  #   end
+  # end
+
+  describe('.all') do
+    it('will show an empty array where all definitions will be held') do
+      expect(Definition.all()).to(eq([]))
     end
   end
 end
